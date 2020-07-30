aa665735d2049265bfb1cc8407eac64a31887dac
Nick <nikitadyshkant@gmail.com>
2020-07-31 02:19:05 +0300

    day 3

--- client/js/radioPlayer.js
+++ client/js/radioPlayer.js
@@ -1,3 +1,59 @@
 export const radioPlayerInit = () =>{
+    const radio = document.querySelector('.radio')
+    const radioNavigation = document.querySelector('.radio-navigation')
+    const radioCoverImg = document.querySelector('.radio-cover__img')
+    const radioItem = document.querySelectorAll('.radio-item')
+    const radioHeaderBig = document.querySelector('.radio-header__big')
+    const radioStop = document.querySelector('.radio-stop')
 
-}
+
+    const audio = new Audio();
+    audio.type = 'audio/aac';
+
+    radioStop.disabled = true;
+
+    const changeIconPlay = () => {
+        if (audio.paused){
+            radio.classList.remove('play')
+            radioStop.classList.add('fa-play')
+            radioStop.classList.remove('fa-stop')
+        } else {
+            radio.classList.add('play')
+            radioStop.classList.add('fa-stop')
+            radioStop.classList.remove('fa-play')
+        }
+    }
+    const selectItem = elem => {
+        radioItem.forEach(item => item.classList.remove('select'));
+        elem.classList.add('select')
+    }
+    //делегирование, пойми это уже
+    radioNavigation.addEventListener('change', event => {
+        const target = event.target;
+        const parent = target.closest('.radio-item');
+
+        selectItem(parent);
+
+        const title = parent.querySelector('.radio-name').textContent;
+        radioHeaderBig.textContent = title
+
+        const img = parent.querySelector('.radio-img').src
+        radioCoverImg.src = img
+
+        radioStop.disabled = false;
+
+        audio.src = target.dataset.radioStantion
+        audio.play()
+        changeIconPlay()
+    })
+
+    radioStop.addEventListener('click', () => {
+        if( audio.paused ){
+
+            audio.play();
+        } else {
+            audio.pause();
+        }
+        changeIconPlay()
+    })
+};
--- client/js/videoPlayer.js
+++ client/js/videoPlayer.js
@@ -2,6 +2,7 @@ export const videoPlayerInit = () =>{
 
 
     const videoPlayer = document.querySelector('.video-player'),
+        videoVolume = document.querySelector('.video-volume'),
         videoButtonPlay = document.querySelector('.video-button__play'),
         videoButtonStop = document.querySelector('.video-button__stop'),
         videoTimePassed = document.querySelector('.video-time__passed'),
@@ -64,4 +65,10 @@ export const videoPlayerInit = () =>{
 
         videoPlayer.currentTime = (value * duration) / 100;
     });
+
+    videoVolume.addEventListener('input', () =>{
+        videoPlayer.volume = videoVolume.value / 100
+    })
+    videoPlayer.volume = 0.5;
+    videoVolume.value = videoPlayer.volume * 100;
 }
